a464181956391f42288079e163ac8c9b253a8ce5
C K Ashby <clay.ashby@gmail.com>
2015-08-15 15:43:58 -0700

    Added MongoDB collection for tasks

--- todoclay.js
+++ todoclay.js
@@ -1,11 +1,11 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "Walk dog" },
-      { text: "Pickup new toaster" },
-      { text: "Flowers for Cindy" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 }
 
