b09a7180b0fa5f791ddf305cff5d1dd2bd0f9105
C K Ashby <clay.ashby@gmail.com>
2015-08-18 07:45:45 -0700

    Added secure methods for add update delete

--- todoclay.js
+++ todoclay.js
@@ -56,3 +56,25 @@ if (Meteor.isClient) {
 
 }
 
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make certain the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  }
+});
